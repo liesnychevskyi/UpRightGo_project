79faaf1383ed506ef639a317c1b91dae84d613b9
liesnychevskyi <liesnychevskyi.s@gmail.com>
2019-05-04 19:44:36 +0300

    Commit 5/4/19

--- src/test/java/examples/flashElementTest.java
+++ src/test/java/examples/flashElementTest.java
@@ -19,13 +19,15 @@ import org.testng.annotations.Test;
 public class flashElementTest
 {
 
-    private String APP = "https://github.com/cloudgrey-io/the-app/releases/download/v1.8.0/TheApp-v1.8.0.apk";
+   // private String APP = "https://github.com/cloudgrey-io/the-app/releases/download/v1.8.0/TheApp-v1.8.0.apk";
+    private String APP = "C://Users//liesn//IdeaProjects//UpRight_Technologies//src//test//java//app//appTest.apk";
 
     private AppiumDriver driver;
     private WebDriverWait wait;
 
-    private By loginScreen = MobileBy.AccessibilityId("Login Screen");
-
+   // private By loginScreen = MobileBy.AccessibilityId("Login Screen");
+    private By okButton = MobileBy.id("");
+    private By apiDemoFlash = MobileBy.xpath("//android.widget.FrameLayout[@resource-id='com.touchboarder.android.api.demos:id/shimmer_view_container']");
     @BeforeTest
     public void setUp() throws IOException
     {
@@ -34,12 +36,12 @@ public class flashElementTest
         caps.setCapability("platformName", "Android");
         caps.setCapability("deviceName", "Samsung 6s");
         caps.setCapability("automationName", "Espresso");
-        caps.setCapability("forceEspressoRebuild", true);
-        caps.setCapability("useKeystore", false);
+       // caps.setCapability("forceEspressoRebuild", true);
+       // caps.setCapability("useKeystore", false);
        // caps.setCapability("keystorePath", "C:\\Users\\liesn\\keysAndroid\\my-release-key.keystore"); // <-- replace with an appropriate path on your filesystem
-        caps.setCapability("keystorePassword", "foo");
-        caps.setCapability("keyAlias", "androiddebugkeystore");
-        caps.setCapability("keyPassword", "foo");
+       // caps.setCapability("keystorePassword", "foo");
+       // caps.setCapability("keyAlias", "androiddebugkeystore");
+       // caps.setCapability("keyPassword", "foo");
 
 
         caps.setCapability("app", APP);
@@ -60,12 +62,13 @@ public class flashElementTest
     @Test
     public void testFlashElement()
     {
-        WebElement el = wait.until(ExpectedConditions.presenceOfElementLocated(loginScreen));
-
+        driver.findElement(By.id("com.touchboarder.android.api.demos:id/buttonDefaultPositive")).click();
+        WebElement el = wait.until(ExpectedConditions.presenceOfElementLocated(apiDemoFlash));
+        //android.view.View[@resource-id='com.uprightpose.upright_go:id/gif_view']
         HashMap<String, Object> scriptArgs = new HashMap<String, Object>();
         scriptArgs.put("element", ((RemoteWebElement)el).getId());
-        scriptArgs.put("durationMillis", 500); // how long should each flash take?
-        scriptArgs.put("repeatCount", 50); // how many times should we flash?
+        scriptArgs.put("durationMillis", 5); // how long should each flash take?
+        scriptArgs.put("repeatCount", 500); // how many times should we flash?
 
         driver.executeScript("mobile: flashElement", scriptArgs);
     }
